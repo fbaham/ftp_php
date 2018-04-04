068a76523adb17cc545c2f85f7c179bd632966e4
strengthtoendure <francisco.bahamondes90@gmail.com>
2018-04-04 20:15:26 -0300

    Header modificado

--- views/header.php
+++ views/header.php
@@ -6,26 +6,53 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="css/estilos.css">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
+  <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
 </head>
 <body>
   <header>
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-      <!-- Brand/logo -->
-      <a class="navbar-brand" href="#">Logo</a>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+		<!-- .container nos permite centrar el contenido de nuestro menu, esta clase es opcional y podemos encerrar el menu <nav> o incluir el contenedor dentro del <nav> -->
+		<div class="container">
+			<!-- Nos sirve para agregar un logotipo al menu -->
+			<a href="#" class="navbar-brand">Sistemas Hipódromo Chile</a>
 
-      <!-- Links -->
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link 1</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link 2</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link 3</a>
-        </li>
-      </ul>
-    </nav>
+			<!-- Nos permite usar el componente collapse para dispositivos moviles -->
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Menu de Navegacion">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbar">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item active">
+						<a href="#" class="nav-link">Inicio <span class="sr-only">(Actual)</span></a>
+					</li>
+					<li class="nav-item dropdown">
+						<a href="#" class="nav-link dropdown-toggle" id="menu-categorias" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Blog
+						</a>
+						<div class="dropdown-menu" aria-labelledby="menu-categorias">
+							<a href="#" class="dropdown-item">HTML</a>
+							<a href="#" class="dropdown-item">CSS</a>
+							<a href="#" class="dropdown-item">JS</a>
+						</div>
+					</li>
+					<li class="nav-item">
+						<a href="#" class="nav-link">Contacto</a>
+					</li>
+
+				</ul>
+				<ul class="navbar-nav navbar-right">
+                    <li class="nav-item">
+                        <a  href="#" class="nav-link"><span class="glyphicon glyphicon-user"></span> Sign Up</a>
+                    </li>
+                    <li class="nav-item">
+                        <a  href="#" class="nav-link"><span class="glyphicon glyphicon-log-in"></span> Login</a>
+                    </li>
+                </ul>
+			</div>
+		</div>
+	</nav>
   </header>
